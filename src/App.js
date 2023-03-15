9875fc92b79ebb74273cfca9abef912ef1a37e59
yash0922 <yashkumarpal987@gmail.com>
2023-03-15 19:07:11 +0530

    update_ProductDetail

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
 import Login from './Component/Login';
+import ProductDetail from './Component/ProductDetail';
 
 function App() {
   return (
     <div className="App">
       
-
+    <ProductDetail/>
     
     </div>
   );
