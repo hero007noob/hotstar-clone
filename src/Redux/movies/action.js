d61c87087874ec56b8be9ccd8d8e5eb032051df1
simantgautam <gautam8178838469@gmail.com>
2023-03-20 19:19:49 +0530

    updTES

--- src/Redux/movies/action.js
+++ src/Redux/movies/action.js
@@ -1,197 +1,206 @@
-import axios from "axios"
+import axios from "axios";
 import { GET_MOVIE_DATA } from "./actionType";
 
 const getMovies = ({ sort, key, language }) => {
-    return (dispatch) => {
-        let url = `https://api.themoviedb.org/3/discover/movie?api_key=939cb94eb1470cd3b74b2ec575a26449&language=en-US&include_adult=false&include_video=false&page=1`;
-        if (sort) {
-            url += `&sort_by=${sort}`
-        }
-        if (language) {
-            url += `&with_original_language=${language}`
-        }
-        try {
-            dispatch(movieData({ loading: true }))
-            axios.get(url)
-                .then((res) => {
-                    console.log('data Action', res.data.results);
-                    return dispatch(movieData({ [key]: res.data.results, loading: false, type: 'movie' }))
-                })
-        } catch (error) {
-            console.log('axios-error', error)
-            dispatch(movieData({ error: error }));
-        }
-
+  return (dispatch) => {
+    let url = `https://api.themoviedb.org/3/discover/movie?api_key=939cb94eb1470cd3b74b2ec575a26449&language=en-US&include_adult=false&include_video=false&page=1`;
+    if (sort) {
+      url += `&sort_by=${sort}`;
     }
-}
+    if (language) {
+      url += `&with_original_language=${language}`;
+    }
+    try {
+      dispatch(movieData({ loading: true }));
+      axios.get(url).then((res) => {
+        console.log("data Action", res.data.results);
+        return dispatch(
+          movieData({ [key]: res.data.results, loading: false, type: "movie" })
+        );
+      });
+    } catch (error) {
+      console.log("axios-error", error);
+      dispatch(movieData({ error: error }));
+    }
+  };
+};
 const getSimilar = ({ id, key }) => {
-    return (dispatch) => {
-        let url = `https://api.themoviedb.org/3/movie/${id}/similar?api_key=939cb94eb1470cd3b74b2ec575a26449&language=en-US&page=1`;
-        try {
-            dispatch(movieData({ loading: true }))
-            axios.get(url)
-                .then((res) => {
-                    console.log('data Action', res.data.results);
-                    return dispatch(movieData({ [key]: res.data.results, loading: false, type: 'movie' }))
-                })
-        } catch (error) {
-            console.log('axios-error', error)
-            dispatch(movieData({ error: error }));
-        }
+  return (dispatch) => {
+    let url = `https://api.themoviedb.org/3/movie/${id}/similar?api_key=939cb94eb1470cd3b74b2ec575a26449&language=en-US&page=1`;
+    try {
+      dispatch(movieData({ loading: true }));
+      axios.get(url).then((res) => {
+        console.log("data Action", res.data.results);
+        return dispatch(
+          movieData({ [key]: res.data.results, loading: false, type: "movie" })
+        );
+      });
+    } catch (error) {
+      console.log("axios-error", error);
+      dispatch(movieData({ error: error }));
     }
-}
+  };
+};
 const searchMovie = ({ query, key }) => {
-    return (dispatch) => {
-        let url = `https://api.themoviedb.org/3/search/movie?api_key=939cb94eb1470cd3b74b2ec575a26449&language=en-US&query=${query}&page=1&include_adult=false`;
-        try {
-            dispatch(movieData({ loading: true }))
-            axios.get(url)
-                .then((res) => {
-                    console.log('data Action', res.data.results);
-                    return dispatch(movieData({ [key]: res.data.results, loading: false, type: 'movie' }))
-                })
-        } catch (error) {
-            console.log('axios-error', error)
-            dispatch(movieData({ error: error }));
-        }
+  return (dispatch) => {
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=939cb94eb1470cd3b74b2ec575a26449&language=en-US&query=${query}&page=1&include_adult=false`;
+    try {
+      // dispatch(movieData({ loading: false }))
+      axios.get(url).then((res) => {
+        console.log("data Action", res.data.results);
+        return dispatch(
+          movieData({ [key]: res.data.results, loading: false, type: "movie" })
+        );
+      });
+    } catch (error) {
+      console.log("axios-error", error);
+      dispatch(movieData({ error: error }));
     }
-}
+  };
+};
 const removeFromWishlist = async (id) => {
+  let config = {
+    method: "delete",
+    maxBodyLength: Infinity,
+    url: `${process.env.REACT_APP_BASE_URL}/wishlist/${id}`,
+    headers: {},
+  };
 
-    let config = {
-        method: 'delete',
-        maxBodyLength: Infinity,
-        url: `${process.env.REACT_APP_BASE_URL}/wishlist/${id}`,
-        headers: {}
-    };
-
-    axios.request(config)
-        .then((response) => {
-            console.log(JSON.stringify(response.data));
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-
-}
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 const addToWishlist = async (body) => {
+  let data = JSON.stringify(body);
 
-    let data = JSON.stringify(body);
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: `${process.env.REACT_APP_BASE_URL}/wishlist`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
 
-    let config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: `${process.env.REACT_APP_BASE_URL}/wishlist`,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        data: data
-    };
-
-    axios.request(config)
-        .then((response) => {
-            console.log(JSON.stringify(response.data));
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-
-}
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 const addToContinue = async (id) => {
-    let url = `https://api.themoviedb.org/3/movie/${id}?api_key=24ca5a64d4833b96467da5ed3580a957&language=en-US`;
-    let res = await axios.get(url);
+  let url = `https://api.themoviedb.org/3/movie/${id}?api_key=24ca5a64d4833b96467da5ed3580a957&language=en-US`;
+  let res = await axios.get(url);
 
-    let data = JSON.stringify(res.data);
+  let data = JSON.stringify(res.data);
 
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: `${process.env.REACT_APP_BASE_URL}/continue`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+const getWishlistData = async (id) => {
+  return new Promise((resolve, reject) => {
     let config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: `${process.env.REACT_APP_BASE_URL}/continue`,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        data: data
+      method: "get",
+      maxBodyLength: Infinity,
+      url: `${process.env.REACT_APP_BASE_URL}/wishlist`,
+      headers: {},
     };
 
-    axios.request(config)
-        .then((response) => {
-            console.log(JSON.stringify(response.data));
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-
-}
-const getWishlistData = async (id) => {
-
-    return new Promise((resolve, reject) => {
-        let config = {
-            method: 'get',
-            maxBodyLength: Infinity,
-            url: `${process.env.REACT_APP_BASE_URL}/wishlist`,
-            headers: {}
-        };
-
-        axios.request(config)
-            .then((response) => {
-                // console.log(JSON.stringify(response.data));
-                resolve(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-
-    });
-}
+    axios
+      .request(config)
+      .then((response) => {
+        // console.log(JSON.stringify(response.data));
+        resolve(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+};
 const getContinueWatching = async (id) => {
+  return new Promise((resolve, reject) => {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: `${process.env.REACT_APP_BASE_URL}/continue`,
+      headers: {},
+    };
 
-    return new Promise((resolve, reject) => {
-        let config = {
-            method: 'get',
-            maxBodyLength: Infinity,
-            url: `${process.env.REACT_APP_BASE_URL}/continue`,
-            headers: {}
-        };
-
-        axios.request(config)
-            .then((response) => {
-                // console.log(JSON.stringify(response.data));
-                resolve([response.data]);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-
-    });
-}
+    axios
+      .request(config)
+      .then((response) => {
+        // console.log(JSON.stringify(response.data));
+        resolve([response.data]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+};
 const checkWishlist = (id) => {
-    return new Promise((resolve, reject) => {
-        let config = {
-            method: 'get',
-            maxBodyLength: Infinity,
-            url: `${process.env.REACT_APP_BASE_URL}/wishlist?id=${id}`,
-            headers: {}
-        };
+  return new Promise((resolve, reject) => {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: `${process.env.REACT_APP_BASE_URL}/wishlist?id=${id}`,
+      headers: {},
+    };
 
-        axios.request(config)
-            .then((response) => {
-                // console.log(JSON.stringify(response.data));
-                if (response.data.length > 0) {
-                    resolve(true);
-                } else {
-                    resolve(false);
-                }
-            })
-            .catch((error) => {
-                resolve(false);
-            });
-    });
-}
+    axios
+      .request(config)
+      .then((response) => {
+        // console.log(JSON.stringify(response.data));
+        if (response.data.length > 0) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      })
+      .catch((error) => {
+        resolve(false);
+      });
+  });
+};
 const movieData = (data) => {
-    return {
-        type: GET_MOVIE_DATA,
-        payload: data
-    }
-}
+  return {
+    type: GET_MOVIE_DATA,
+    payload: data,
+  };
+};
 
-export { getMovies, getSimilar, removeFromWishlist, addToWishlist, checkWishlist, getWishlistData, searchMovie, getContinueWatching, addToContinue }
+export {
+  getMovies,
+  getSimilar,
+  removeFromWishlist,
+  addToWishlist,
+  checkWishlist,
+  getWishlistData,
+  searchMovie,
+  getContinueWatching,
+  addToContinue,
+};
--- src/Redux/parentRedux/action.js
+++ src/Redux/parentRedux/action.js
@@ -1,66 +1,69 @@
 import axios from "axios";
 
-const GET_PARENT_CONTROLS = 'GET_PARENT_CONTROLS'
+const GET_PARENT_CONTROLS = "GET_PARENT_CONTROLS";
 const setParentControls = ({ value }) => {
-    console.log('val', value)
-    return (dispatch) => {
-        return dispatch(parentData({ loading: false, isLocked: value }));
-    }
-}
+  console.log("val", value);
+  return (dispatch) => {
+    return dispatch(parentData({ loading: false, isLocked: value }));
+  };
+};
 
 const getParentControls = async () => {
+  return new Promise((resolve, reject) => {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: `${process.env.REACT_APP_BASE_URL}/parentalLock`,
+      headers: {},
+    };
 
-    return new Promise((resolve, reject) => {
-        let config = {
-            method: 'get',
-            maxBodyLength: Infinity,
-            url: `${process.env.REACT_APP_BASE_URL}/parentalLock`,
-            headers: {}
-        };
-
-        axios.request(config)
-            .then((response) => {
-                console.log('lock status res', JSON.stringify(response.data));
-                resolve(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-
-    });
-}
+    axios
+      .request(config)
+      .then((response) => {
+        console.log("lock status res", JSON.stringify(response.data));
+        resolve(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+};
 export const updateParentControls = async (status) => {
-
-    return new Promise((resolve, reject) => {
-        let data = JSON.stringify({
-            "status": status
-        });
-
-        let config = {
-            method: 'post',
-            maxBodyLength: Infinity,
-            url: 'http://localhost:4000/parentalLock/',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            data: data
-        };
-
-        axios.request(config)
-            .then((response) => {
-                console.log(JSON.stringify(response.data));
-                resolve();
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-
+  return new Promise((resolve, reject) => {
+    let data = JSON.stringify({
+      status: status,
     });
-}
+
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: `${process.env.REACT_APP_BASE_URL}/parentalLock`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        resolve();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+};
 const parentData = (data) => {
-    return {
-        type: GET_PARENT_CONTROLS,
-        payload: data
-    }
-}
-export { setParentControls, parentData, GET_PARENT_CONTROLS, getParentControls }
+  return {
+    type: GET_PARENT_CONTROLS,
+    payload: data,
+  };
+};
+export {
+  setParentControls,
+  parentData,
+  GET_PARENT_CONTROLS,
+  getParentControls,
+};
